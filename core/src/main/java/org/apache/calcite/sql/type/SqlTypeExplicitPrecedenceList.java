aa9f9365beacd8a5c396aa0231e2b9e5db470382
Xuecheng Shan <xuecheng.shan@kyligence.io>
2025-05-28 11:27:38 +0800

    AL-10159 fix function params type match

--- core/src/main/java/org/apache/calcite/sql/type/SqlTypeExplicitPrecedenceList.java
+++ core/src/main/java/org/apache/calcite/sql/type/SqlTypeExplicitPrecedenceList.java
@@ -138,23 +138,31 @@ public class SqlTypeExplicitPrecedenceList
 
   // implement RelDataTypePrecedenceList
   @Override public int compareTypePrecedence(RelDataType type1, RelDataType type2) {
-    assert containsType(type1) : type1;
-    assert containsType(type2) : type2;
+    if (SqlTypeName.ANY != type1.getSqlTypeName()) {
+      assert containsType(type1) : type1;
+    }
+    if (SqlTypeName.ANY != type2.getSqlTypeName()) {
+      assert containsType(type2) : type2;
+    }
 
     int p1 =
         getListPosition(
             type1.getSqlTypeName(),
             typeNames);
+    p1 = (p1 == -1) ? Integer.MAX_VALUE : p1;
     int p2 =
         getListPosition(
             type2.getSqlTypeName(),
             typeNames);
+    p2 = (p2 == -1) ? Integer.MAX_VALUE : p2;
     return p2 - p1;
   }
 
   private static int getListPosition(SqlTypeName type, List<SqlTypeName> list) {
     int i = list.indexOf(type);
-    assert i != -1;
+    if (SqlTypeName.ANY != type) {
+      assert i != -1;
+    }
     return i;
   }
 
